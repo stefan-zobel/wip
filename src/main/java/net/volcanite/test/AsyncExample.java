2d11d8dc306c31202fa88f9edc3f356521a357c2
stefan-zobel <stefan-zobel@users.noreply.github.com>
2020-05-21 19:38:38 +0200

    AsyncExample

---
+++ src/main/java/net/volcanite/test/AsyncExample.java
@@ -0,0 +1,42 @@
+package net.volcanite.test;
+
+import static net.volcanite.test.TestUtil.randomBytes;
+
+import net.volcanite.persistence.TransmitTask;
+import net.volcanite.task.AsyncExecutor;
+
+public class AsyncExample {
+
+	public static void main(String[] args) {
+
+		AsyncExecutor.start();
+		int RUNS = 500_000;
+		long runtime = 0L;
+
+		for (int i = 0; i < RUNS; ++i) {
+			long start = System.currentTimeMillis();
+			byte[] key = randomBytes();
+			byte[] value = randomBytes();
+
+			AsyncExecutor.execute(new TransmitTask(key, value));
+			runtime += (System.currentTimeMillis() - start);
+		}
+
+		System.out.println("runti>  avg: " + (runtime / (double) RUNS) + " ms");
+		pause(13);
+		AsyncExecutor.stop();
+	}
+
+	private static void pause(int seconds) {
+		Object o = new Object();
+		try {
+			synchronized (o) {
+				System.err.println("waiting");
+				o.wait(1000L * seconds);
+				System.err.println("done");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
