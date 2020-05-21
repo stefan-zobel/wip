faa1de75dba05297558fc75052a0a5dcda0adc7d
stefan-zobel <stefan-zobel@users.noreply.github.com>
2020-05-21 13:53:04 +0200

    DBException

---
+++ src/main/java/net/volcanite/db/DBException.java
@@ -0,0 +1,25 @@
+package net.volcanite.db;
+
+public class DBException extends RuntimeException {
+
+    private static final long serialVersionUID = 7935298121550955744L;
+
+    public DBException() {
+    }
+
+    public DBException(String message) {
+        super(message);
+    }
+
+    public DBException(Throwable cause) {
+        super(cause);
+    }
+
+    public DBException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DBException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
