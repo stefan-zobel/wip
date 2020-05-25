f8dccbce6416bed246f2acf5fb79cf90f09b6597
stefan-zobel <stefan-zobel@users.noreply.github.com>
2020-05-25 13:26:50 +0200

    whitespace

--- src/main/java/net/volcanite/db/Get.java
+++ src/main/java/net/volcanite/db/Get.java
@@ -8,44 +8,44 @@ import net.volcanite.util.DoubleStatistics;
 
 public class Get extends DB {
 
-	private final DoubleStatistics readTimeNanos = DoubleStatistics.newInstance();
+    private final DoubleStatistics readTimeNanos = DoubleStatistics.newInstance();
 
-	private final ReadOptions readOptions;
+    private final ReadOptions readOptions;
 
-	public Get(String dbPath) {
-		super(dbPath);
-		readOptions = new ReadOptions();
-	}
+    public Get(String dbPath) {
+        super(dbPath);
+        readOptions = new ReadOptions();
+    }
 
-	@Override
-	public void close() {
-		try {
-			super.close();
-		} finally {
-			close(readOptions);
-		}
-	}
+    @Override
+    public void close() {
+        try {
+            super.close();
+        } finally {
+            close(readOptions);
+        }
+    }
 
-	public byte[] read(byte[] key) {
-		try {
-			return read(key, db.txnDb, readOptions);
-		} catch (RocksDBException e) {
-			throw new DBException(e);
-		}
-	}
+    public byte[] read(byte[] key) {
+        try {
+            return read(key, db.txnDb, readOptions);
+        } catch (RocksDBException e) {
+            throw new DBException(e);
+        }
+    }
 
-	private byte[] read(byte[] key, TransactionDB txnDb, ReadOptions readOptions)
-			throws RocksDBException {
+    private byte[] read(byte[] key, TransactionDB txnDb, ReadOptions readOptions)
+            throws RocksDBException {
 
-		long readStart = System.nanoTime();
-		try {
-			return txnDb.get(readOptions, key);
-		} finally {
-			readTimeNanos.accept(System.nanoTime() - readStart);
-		}
-	}
+        long readStart = System.nanoTime();
+        try {
+            return txnDb.get(readOptions, key);
+        } finally {
+            readTimeNanos.accept(System.nanoTime() - readStart);
+        }
+    }
 
-	public DoubleStatistics getReadTimeNanos() {
-		return readTimeNanos;
-	}
+    public DoubleStatistics getReadTimeNanos() {
+        return readTimeNanos;
+    }
 }
