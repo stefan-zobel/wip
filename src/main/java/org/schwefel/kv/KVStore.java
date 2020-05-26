3b8a4545279410cf9613b79a6f4a39639fe0cbc9
stefan-zobel <stefan-zobel@users.noreply.github.com>
2020-05-26 13:07:24 +0200

    whitespace

--- src/main/java/org/schwefel/kv/KVStore.java
+++ src/main/java/org/schwefel/kv/KVStore.java
@@ -14,118 +14,120 @@ import org.rocksdb.WriteOptions;
 
 public class KVStore implements StoreOps {
 
-	private static final Logger logger = Logger.getLogger(KVStore.class.getName());
+    private static final Logger logger = Logger.getLogger(KVStore.class.getName());
 
-	private volatile boolean open = false;
+    private static final int PARALLELISM = Math.max(Runtime.getRuntime().availableProcessors(), 2);
 
-	private TransactionDB txnDb;
-	private TransactionDBOptions txnDbOptions;
-	private TransactionOptions txnOpts;
-	private final WriteOptions writeOptions;
-	private final ReadOptions readOptions;
-	private final FlushOptions flushOptions;
+    private volatile boolean open = false;
 
-	public KVStore(Path dir) {
-		// TODO Auto-generated constructor stub
-		writeOptions = new WriteOptions(); // XXX ???
-		readOptions = new ReadOptions(); // XXX ???
-		flushOptions = new FlushOptions(); // XXX ???
-	}
+    private TransactionDB txnDb;
+    private TransactionDBOptions txnDbOptions;
+    private TransactionOptions txnOpts;
+    private final WriteOptions writeOptions;
+    private final ReadOptions readOptions;
+    private final FlushOptions flushOptions;
 
-	@Override
-	public void close() {
-		if (!isOpen()) {
-			return;
-		}
-		try {
-			syncWAL();
-		} catch (Exception e) {
-			logger.log(Level.INFO, "", e);
-		}
-		try {
-			flush();
-		} catch (Exception e) {
-			logger.log(Level.INFO, "", e);
-		}
-		open = false;
+    public KVStore(Path dir) {
+        // TODO Auto-generated constructor stub
+        writeOptions = new WriteOptions(); // XXX ???
+        readOptions = new ReadOptions(); // XXX ???
+        flushOptions = new FlushOptions(); // XXX ???
+    }
+
+    @Override
+    public synchronized void close() {
+        if (!isOpen()) {
+            return;
+        }
+        try {
+            syncWAL();
+        } catch (Exception e) {
+            logger.log(Level.INFO, "", e);
+        }
+        try {
+            flush();
+        } catch (Exception e) {
+            logger.log(Level.INFO, "", e);
+        }
+        open = false;
         close(txnDb);
         close(txnDbOptions);
         close(txnOpts);
         close(writeOptions);
         close(readOptions);
         close(flushOptions);
-	}
+    }
 
-	@Override
-	public void put(byte[] key, byte[] value) {
-		// TODO Auto-generated method stub
+    @Override
+    public synchronized  void put(byte[] key, byte[] value) {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
-	@Override
-	public void putIfAbsent(byte[] key, byte[] value) {
-		// TODO Auto-generated method stub
+    @Override
+    public synchronized void putIfAbsent(byte[] key, byte[] value) {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
-	@Override
-	public byte[] get(byte[] key) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public synchronized byte[] get(byte[] key) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public void delete(byte[] key) {
-		// TODO Auto-generated method stub
+    @Override
+    public synchronized void delete(byte[] key) {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
-	@Override
-	public void deleteRange(byte[] beginKey, byte[] endKey) {
-		// TODO Auto-generated method stub
+    @Override
+    public synchronized void deleteRange(byte[] beginKey, byte[] endKey) {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
-	@Override
-	public void update(byte[] key, byte[] value) {
-		// TODO Auto-generated method stub
+    @Override
+    public synchronized void update(byte[] key, byte[] value) {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
-	@Override
-	public void writeBatch(Batch batch) {
-		// TODO Auto-generated method stub
+    @Override
+    public synchronized void writeBatch(Batch batch) {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
-	@Override
-	public void syncWAL() {
-		if (isOpen()) {
-			try {
-				if (txnDb.isOwningHandle()) {
-					txnDb.flushWal(true);
-				}
-			} catch (RocksDBException e) {
-				throw new StoreException(e);
-			}
-		}
-	}
+    @Override
+    public synchronized void syncWAL() {
+        if (isOpen()) {
+            try {
+                if (txnDb.isOwningHandle()) {
+                    txnDb.flushWal(true);
+                }
+            } catch (RocksDBException e) {
+                throw new StoreException(e);
+            }
+        }
+    }
 
-	@Override
-	public boolean isOpen() {
-		return open;
-	}
+    @Override
+    public boolean isOpen() {
+        return open;
+    }
 
-	@Override
-	public void flush() {
-		if (isOpen()) {
-			try {
-				txnDb.flush(flushOptions);
-			} catch (RocksDBException e) {
-				throw new StoreException(e);
-			}
-		}
-	}
+    @Override
+    public synchronized void flush() {
+        if (isOpen()) {
+            try {
+                txnDb.flush(flushOptions);
+            } catch (RocksDBException e) {
+                throw new StoreException(e);
+            }
+        }
+    }
 
     private static void close(AutoCloseable ac) {
         if (ac != null) {
