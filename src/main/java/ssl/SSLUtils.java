ccf8b7248ebd76629c93e8c06af04c26f46d4d98
stefan-zobel <stefan-zobel@users.noreply.github.com>
2020-07-10 00:06:46 +0200

    add 2 fallbacks for older Java 8 VMs

--- src/main/java/ssl/SSLUtils.java
+++ src/main/java/ssl/SSLUtils.java
@@ -19,7 +19,9 @@ public final class SSLUtils {
                             "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384",
                             "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256",
                             "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384",
-                            "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256");
+                            "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256",
+                            "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256",
+                            "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256");
     //@formatter:on
 
     private static List<String> safestSuites = Collections.emptyList();
