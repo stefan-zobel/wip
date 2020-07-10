71103f4f3fe86e0c1c9680a1e2b409bfa358ee46
stefan-zobel <stefan-zobel@users.noreply.github.com>
2020-07-11 01:02:36 +0200

    refactor

--- src/main/java/ks/KSLoader.java
+++ src/main/java/ks/KSLoader.java
@@ -1,12 +1,17 @@
 package ks;
 
+import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
+
+import misc.PBKDF2;
 
 public final class KSLoader {
 
@@ -27,7 +32,28 @@ public final class KSLoader {
         }
     }
 
+    public static KeyStore loadKeyStore(String password, Path file) {
+        //@formatter:off
+        try (InputStream is = Files.newInputStream(file);
+             BufferedInputStream bis = new BufferedInputStream(is, 16384)
+        )
+        {
+            KeyStore ks = KeyStore.getInstance("PKCS12");
+            ks.load(bis, passPhrase(password));
+            return ks;
+        } catch (KeyStoreException | CertificateException | NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+        //@formatter:on
+    }
+
     public static char[] passPhrase() {
         return "VpAovsDbxZdX+k3qed8hqEOeSqknJGT1uYanS69gZ2CBJ6oQuZSCGYky7n4WZGpWNWwR".toCharArray();
+    }
+
+    private static char[] passPhrase(String password) {
+        return PBKDF2.getKey(password);
     }
 }
