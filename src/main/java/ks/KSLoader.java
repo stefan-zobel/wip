4d594291ef5ef5368a14c0ec751923c874c3f0e7
stefan-zobel <stefan-zobel@users.noreply.github.com>
2020-07-11 00:24:22 +0200

    update KeyStore

--- src/main/java/ks/KSLoader.java
+++ src/main/java/ks/KSLoader.java
@@ -10,8 +10,13 @@ import java.security.cert.CertificateException;
 
 public final class KSLoader {
 
+    private static final String KS_NAME = "ks.p12";
+
     public static KeyStore loadKeyStore() {
-        try (InputStream is = KSLoader.class.getResourceAsStream("ks.p12")) {
+        try (InputStream is = KSLoader.class.getResourceAsStream(KS_NAME)) {
+            if (is == null) {
+                throw new IllegalStateException("KeyStore " + KS_NAME + " not found!");
+            }
             KeyStore ks = KeyStore.getInstance("PKCS12");
             ks.load(is, passPhrase());
             return ks;
@@ -23,6 +28,6 @@ public final class KSLoader {
     }
 
     public static char[] passPhrase() {
-        return "passphrase".toCharArray();
+        return "VpAovsDbxZdX+k3qed8hqEOeSqknJGT1uYanS69gZ2CBJ6oQuZSCGYky7n4WZGpWNWwR".toCharArray();
     }
 }
