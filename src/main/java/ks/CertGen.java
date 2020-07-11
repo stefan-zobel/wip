c80cb80b5e5f9825f663682ef4156aad48ae6680
stefan-zobel <stefan-zobel@users.noreply.github.com>
2020-07-11 18:22:50 +0200

    add ECDSA certificate

--- src/main/java/ks/CertGen.java
+++ src/main/java/ks/CertGen.java
@@ -36,51 +36,90 @@ import sun.security.x509.X500Name;
 
 public final class CertGen {
 
-    private static final int KEY_BITS_DEFAULT = 5120;
+    private static final int RSA_KEY_BITS_DEFAULT = 5120;
+    private static final int ECDSA_KEY_BITS_DEFAULT = 521;
+    // 15 years validity
+    private static final long VALIDITY = 15L * 365L * 24L * 60L * 60L;
+    private static final String DUMMY_DN = "CN=GeoTrust Primary Certification Authority - G2, OU=(c) 2007 GeoTrust Inc. - For authorized use only,  O=GeoTrust Inc., C=US";
+    private static final String DEFAULT_ALIAS = "localhost";
 
     public static void main(String[] args) {
         String password = (args != null && args.length > 0) ? args[0] : "";
         String ksFileName = (args != null && args.length > 1) ? args[1] : "ks.p12";
+        String dn = (args != null && args.length > 2) ? args[2] : DUMMY_DN;
+        String alias = (args != null && args.length > 3) ? args[3] : DEFAULT_ALIAS;
         char[] passwd = PBKDF2.getKey(password);
         System.out.println(passwd);
         System.out.println(passwd.length);
-        String dn = "CN=GeoTrust Primary Certification Authority - G2, OU=(c) 2007 GeoTrust Inc. - For authorized use only,  O=GeoTrust Inc., C=US";
-        String alias = "localhost";
 
         long start = System.currentTimeMillis();
 
-        KeyStore ks = generateKeyStore(dn, KEY_BITS_DEFAULT, alias, passwd);
+        Date startDate = new Date();
+        KeyStore ks = generateKeyStore(dn, alias, passwd, startDate);
 
         long end = System.currentTimeMillis();
         System.out.println("took: " + (end - start) + " ms");
 
-        try {
-            ks.store(new FileOutputStream(ksFileName), passwd);
+        try (FileOutputStream fos = new FileOutputStream(ksFileName)) {
+            ks.store(fos, passwd);
         } catch (KeyStoreException | NoSuchAlgorithmException | CertificateException | IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private static KeyStore generateKeyStore(String certificateDN, int keyBits, String keyEntryAlias, char[] passwd) {
+    private static KeyStore generateKeyStore(String certificateDN, String keyEntryAlias, char[] passwd,
+            Date startDate) {
         try {
             KeyStore keyStore = KeyStore.getInstance("PKCS12");
             keyStore.load(null, new char[] {});
-            CertAndKeyGen certGen = new CertAndKeyGen("RSA", "SHA512withRSA", null);
-            certGen.generate(keyBits);
 
+            generateECDSACertificate(keyStore, certificateDN, keyEntryAlias, passwd, startDate);
+            generateRSACertificate(keyStore, certificateDN, keyEntryAlias + "2", passwd, startDate);
+
+            return keyStore;
+        } catch (KeyStoreException | NoSuchAlgorithmException | CertificateException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void generateECDSACertificate(KeyStore keyStore, String certificateDN, String keyEntryAlias,
+            char[] passwd, Date startDate) {
+        try {
+            CertAndKeyGen certGen = new CertAndKeyGen("EC", "SHA512withECDSA", null);
+            certGen.generate(ECDSA_KEY_BITS_DEFAULT);
+
+            storeCertificate(keyStore, certificateDN, keyEntryAlias, passwd, startDate, certGen);
+
+        } catch (InvalidKeyException | NoSuchAlgorithmException | NoSuchProviderException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void generateRSACertificate(KeyStore keyStore, String certificateDN, String keyEntryAlias,
+            char[] passwd, Date startDate) {
+        try {
+            CertAndKeyGen certGen = new CertAndKeyGen("RSA", "SHA512withRSA", null);
+            certGen.generate(RSA_KEY_BITS_DEFAULT);
+
+            storeCertificate(keyStore, certificateDN, keyEntryAlias, passwd, startDate, certGen);
+
+        } catch (InvalidKeyException | NoSuchAlgorithmException | NoSuchProviderException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void storeCertificate(KeyStore keyStore, String certificateDN, String keyEntryAlias, char[] passwd,
+            Date startDate, CertAndKeyGen certGen) {
+        try {
             CertificateExtensions ext = new CertificateExtensions();
             ext.set(SubjectKeyIdentifierExtension.NAME,
                     new SubjectKeyIdentifierExtension(new KeyIdentifier(certGen.getPublicKeyAnyway()).getIdentifier()));
 
-            // 15 years validity
-            long validSecs = 15L * 365L * 24L * 60L * 60L;
-            X509Certificate cert = certGen.getSelfCertificate(new X500Name(certificateDN), new Date(), validSecs, ext);
+            X509Certificate cert = certGen.getSelfCertificate(new X500Name(certificateDN), startDate, VALIDITY, ext);
 
             keyStore.setKeyEntry(keyEntryAlias, certGen.getPrivateKey(), passwd, new X509Certificate[] { cert });
-
-            return keyStore;
-        } catch (InvalidKeyException | KeyStoreException | NoSuchAlgorithmException | CertificateException
-                | NoSuchProviderException | SignatureException | IOException e) {
+        } catch (InvalidKeyException | CertificateException | SignatureException | NoSuchAlgorithmException
+                | NoSuchProviderException | KeyStoreException | IOException e) {
             throw new RuntimeException(e);
         }
     }
