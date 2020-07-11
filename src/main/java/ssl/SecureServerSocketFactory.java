70398016723efe45f0596757488a99eea65b81b2
stefan-zobel <stefan-zobel@users.noreply.github.com>
2020-07-12 01:03:56 +0200

    Secure(Server)SocketFactory

---
+++ src/main/java/ssl/SecureServerSocketFactory.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2020 Stefan Zobel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ssl;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.nio.file.Path;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLParameters;
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
+
+public class SecureServerSocketFactory extends SSLServerSocketFactory {
+
+    private static final SecureServerSocketFactory defaultFactory = new SecureServerSocketFactory();
+
+    private final SSLContext ctx;
+    private final SSLServerSocketFactory factory;
+
+    private SecureServerSocketFactory() {
+        ctx = new SimpleSSLContext().get();
+        factory = ctx.getServerSocketFactory();
+    }
+
+    public SecureServerSocketFactory(String password, Path keystore) {
+        ctx = SimpleSSLContext.get(password, keystore);
+        factory = ctx.getServerSocketFactory();
+    }
+
+    /**
+     * Returns the default SecureServerSocketFactory.
+     *
+     * @return the default {@code SecureServerSocketFactory}
+     */
+    public static SecureServerSocketFactory getDefault() {
+        return defaultFactory;
+    }
+
+    @Override
+    public String[] getDefaultCipherSuites() {
+        return getSupportedCipherSuites();
+    }
+
+    @Override
+    public String[] getSupportedCipherSuites() {
+        return SSLUtils.allSafeCipherSuites();
+    }
+
+    @Override
+    public SSLServerSocket createServerSocket() throws IOException {
+        return prepareSocket(factory.createServerSocket());
+    }
+
+    @Override
+    public SSLServerSocket createServerSocket(int port) throws IOException {
+        return prepareSocket(factory.createServerSocket(port));
+    }
+
+    @Override
+    public SSLServerSocket createServerSocket(int port, int backlog) throws IOException {
+        return prepareSocket(factory.createServerSocket(port, backlog));
+    }
+
+    @Override
+    public SSLServerSocket createServerSocket(int port, int backlog, InetAddress ifAddress) throws IOException {
+        return prepareSocket(factory.createServerSocket(port, backlog, ifAddress));
+    }
+
+    private SSLServerSocket prepareSocket(ServerSocket sock) throws IOException {
+        SSLServerSocket sslSock = (SSLServerSocket) sock;
+        sslSock.setReuseAddress(true);
+        sslSock.setEnabledProtocols(SSLUtils.allSafeProtocols());
+        sslSock.setEnabledCipherSuites(getDefaultCipherSuites());
+        SSLParameters params = sslSock.getSSLParameters();
+        params.setUseCipherSuitesOrder(true);
+        sslSock.setSSLParameters(params);
+        return sslSock;
+    }
+}
---
+++ src/main/java/ssl/SecureSocketFactory.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2020 Stefan Zobel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ssl;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.nio.file.Path;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+
+public class SecureSocketFactory extends SSLSocketFactory {
+
+    private static final SecureSocketFactory defaultFactory = new SecureSocketFactory();
+
+    private final SSLContext ctx;
+    private final SSLSocketFactory factory;
+
+    private SecureSocketFactory() {
+        ctx = new SimpleSSLContext().get();
+        factory = ctx.getSocketFactory();
+    }
+
+    public SecureSocketFactory(String password, Path keystore) {
+        ctx = SimpleSSLContext.get(password, keystore);
+        factory = ctx.getSocketFactory();
+    }
+
+    /**
+     * Returns the default SecureSocketFactory.
+     *
+     * @return the default {@code SecureSocketFactory}
+     */
+    public static SecureSocketFactory getDefault() {
+        return defaultFactory;
+    }
+
+    @Override
+    public String[] getDefaultCipherSuites() {
+        return getSupportedCipherSuites();
+    }
+
+    @Override
+    public String[] getSupportedCipherSuites() {
+        return SSLUtils.allSafeCipherSuites();
+    }
+
+    @Override
+    public SSLSocket createSocket() throws IOException {
+        return prepareSocket(factory.createSocket());
+    }
+
+    @Override
+    public SSLSocket createSocket(Socket s, String host, int port, boolean autoClose) throws IOException {
+        return prepareSocket(factory.createSocket(s, host, port, autoClose));
+    }
+
+    @Override
+    public SSLSocket createSocket(String host, int port) throws IOException, UnknownHostException {
+        return prepareSocket(factory.createSocket(host, port));
+    }
+
+    @Override
+    public SSLSocket createSocket(InetAddress host, int port) throws IOException {
+        return prepareSocket(factory.createSocket(host, port));
+    }
+
+    @Override
+    public SSLSocket createSocket(String host, int port, InetAddress localHost, int localPort)
+            throws IOException, UnknownHostException {
+        return prepareSocket(factory.createSocket(host, port, localHost, localPort));
+    }
+
+    @Override
+    public SSLSocket createSocket(InetAddress address, int port, InetAddress localAddress, int localPort)
+            throws IOException {
+        return prepareSocket(factory.createSocket(address, port, localAddress, localPort));
+    }
+
+    @Override
+    public SSLSocket createSocket(Socket s, InputStream consumed, boolean autoClose) throws IOException {
+        return prepareSocket(factory.createSocket(s, consumed, autoClose));
+    }
+
+    private SSLSocket prepareSocket(Socket sock) throws IOException {
+        SSLSocket sslSock = (SSLSocket) sock;
+        sslSock.setReuseAddress(true);
+        sslSock.setEnabledProtocols(SSLUtils.allSafeProtocols());
+        sslSock.setEnabledCipherSuites(getDefaultCipherSuites());
+        return sslSock;
+    }
+}
