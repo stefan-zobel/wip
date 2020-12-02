1fbc5cd508154b2468f876d5d095687abc61288e
stefan-zobel <stefan-zobel@users.noreply.github.com>
2020-12-03 00:58:42 +0100

    format #ifdef __linux section

--- src/main/java/mmap/impl/Native.cpp
+++ src/main/java/mmap/impl/Native.cpp
@@ -12,21 +12,21 @@
 #endif
 
 #ifdef __linux
-#ifdef _LP64
-#ifndef jlong_to_ptr
-#define jlong_to_ptr(a) ((void*)(a))
-#endif
-#ifndef ptr_to_jlong
-#define ptr_to_jlong(a) ((jlong)(a))
-#endif
-#else
-#ifndef jlong_to_ptr
-#define jlong_to_ptr(a) ((void*)(int)(a))
-#endif
-#ifndef ptr_to_jlong
-#define ptr_to_jlong(a) ((jlong)(int)(a))
-#endif
-#endif
+  #ifdef _LP64
+    #ifndef jlong_to_ptr
+      #define jlong_to_ptr(a) ((void*)(a))
+    #endif
+    #ifndef ptr_to_jlong
+      #define ptr_to_jlong(a) ((jlong)(a))
+    #endif
+  #else
+    #ifndef jlong_to_ptr
+      #define jlong_to_ptr(a) ((void*)(int)(a))
+    #endif
+    #ifndef ptr_to_jlong
+      #define ptr_to_jlong(a) ((jlong)(int)(a))
+    #endif
+  #endif
 #endif
 
 
