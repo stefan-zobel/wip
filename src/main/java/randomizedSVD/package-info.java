c851f98b6e4ef8ee2b32fe4f5134087dddb83db3
stefan-zobel <stefan-zobel@users.noreply.github.com>
2021-02-03 21:02:56 +0100

    package-info

---
+++ src/main/java/randomizedSVD/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Some randomized SVD algorithms from Nathan Halko, Per-Gunnar Martinsson, and
+ * Joel A Tropp. Finding structure with randomness: Probabilistic algorithms for
+ * constructing approximate matrix decompositions. SIAM review, 53(2):217–288,
+ * 2011.
+ */
+package randomizedSVD;
