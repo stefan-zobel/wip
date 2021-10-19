900ef819a70ff614e066d9466e147993a491cdb1
stefan-zobel <stefan-zobel@users.noreply.github.com>
2021-10-19 22:21:39 +0200

    test cosmetics

--- src/test/java/async/AsyncSourceExample.java
+++ src/test/java/async/AsyncSourceExample.java
@@ -1,5 +1,7 @@
 package async;
 
+import java.util.Objects;
+
 public class AsyncSourceExample extends AbstractAsyncSource<Integer> {
 
     static final class Agent implements AsyncSourceAgent<Integer> {
@@ -26,13 +28,13 @@ public class AsyncSourceExample extends AbstractAsyncSource<Integer> {
 
         @Override
         public void onInit(CancellationToken cancellationToken) {
-            ct = cancellationToken;
+            ct = Objects.requireNonNull(cancellationToken);
         }
     }
 
     public static void main(String[] args) throws InterruptedException {
         AsyncSourceAgent<Integer> agent = new Agent();
-        AsyncSourceExample source = new AsyncSourceExample();
+        AsyncSourceExample source = new AsyncSourceExample(5, 101, 250L);
 
         CancellationToken ct = source.forEach(agent);
 
@@ -43,9 +45,19 @@ public class AsyncSourceExample extends AbstractAsyncSource<Integer> {
         System.out.println("done");
     }
 
+    private final int start;
+    private final int stop;
+    private final long sleep;
+
+    public AsyncSourceExample(int start, int stop, long sleep) {
+        this.start = start;
+        this.stop = stop;
+        this.sleep = sleep;
+    }
+
     @Override
     protected Runnable newRunnable(AsyncSourceAgent<Integer> agent, CancellationToken ct) {
-        return new Producer(5, 101, agent, 250L, ct);
+        return new Producer(start, stop, agent, sleep, ct);
     }
 
     static final class Producer implements Runnable {
